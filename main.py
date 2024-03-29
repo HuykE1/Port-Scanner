449e00dc50c492dc24e094a9f68f6f0f38e620ff
HuykE1 <huykmane@gmail.com>
2024-03-29 14:47:04 +0000

    input IP of choice, won't finish

--- main.py
+++ main.py
@@ -1,33 +1,38 @@
 import socket
 import threading
-import queue
 import pyfiglet
+import queue
 
 banner = pyfiglet.figlet_format("PORT SCANNER")
 print(banner)
 
-
-IP= '192.168.0.38'
 q = queue.Queue()
+
+getIP = input("Enter a remote host to scan: ")
+IPgot = socket.gethostbyname(getIP)
+
+print("_" * 70)
+print("Scanning, please wait")
+print("_" * 70)
+
 
 for i in range(1, 1001):
     q.put(i)
 
 
 def scan():
-    while not q.empty():
-        port = q.get()
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-            try:
-              s.connect((IP, port))
-              print(f'port {port} is Open')
-            except: 
-                  pass
-        q.task_done()
-    
+     while not q.empty():
+         port = q.get()
+         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+             try:
+                 s.connect((IPgot, port))
+                 print(f'port {port} is Open')
+             except: 
+                    pass
+                    q.task_done()
 
-for i in range(30):
-    t = threading.Thread(target= scan, daemon= True)
+for i in range(1001):
+    t = threading.Thread(target= scan, daemon=False)
     t.start()
 
 q.join()
