b72ec67d209023bea334862712437cbf36cc8e17
HuykE1 <huykmane@gmail.com>
2024-03-29 15:46:57 +0000

    Annotated code

--- main.py
+++ main.py
@@ -1,26 +1,26 @@
-import socket
+import socket           #importing all modules required for code
 import threading
 import pyfiglet
 import queue
 
-banner = pyfiglet.figlet_format("PORT SCANNER")
+banner = pyfiglet.figlet_format("PORT SCANNER")         #Creating the banner title for the port scanner
 print(banner)
 
 q = queue.Queue()
 
-getIP = input("Enter a remote host to scan: ")
-IPgot = socket.gethostbyname(getIP)
+getIP = input("Enter a remote host to scan: ")          #Allowing the user to input selected IP address
+IPgot = socket.gethostbyname(getIP)         #Retrieving the inputted IP
 
-print("_" * 70)
+print("_" * 70)         #Creating lines and showing process is starting
 print("Scanning, please wait")
 print("_" * 70)
 
 
-for i in range(1, 1001):
+for i in range(1, 1001):            #Putting elements into the queue
     q.put(i)
 
 
-def scan():
+def scan():         #Function that carries out the scan by eliminating all treads
      while not q.empty():
          port = q.get()
          with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
@@ -31,9 +31,9 @@ def scan():
                     pass
              q.task_done()
 
-for i in range(1001):
+for i in range(1001):           #Number of threads wished to use
     t = threading.Thread(target= scan, daemon=False)
     t.start()
 
-q.join()
+q.join()            #Ensures all elements in the queue have their tasks done before ending the application
 print('Scan complete')
